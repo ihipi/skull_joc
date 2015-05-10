10c55bb35b7a80146380590556461136592139fc
albert <albert.giro@gmail.com>
2015-05-10 10:01:11 -0600

    projecte inicial de joc

---
+++ desktop/src/com/skull/joc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.skull.joc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.skull.joc.skulljoc;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new skulljoc(), config);
+	}
+}
